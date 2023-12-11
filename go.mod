0d7faea16d0878b63e736a7cd549e305b715432f
Akmyrzza <akmyrzza@gmail.com>
2023-12-11 18:10:18 +0600

    iter12

--- go.mod
+++ go.mod
@@ -20,7 +20,9 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
+	github.com/go-resty/resty/v2 v2.10.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
@@ -38,10 +40,12 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.16.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/server/handler/handler_test.go
+++ internal/server/handler/handler_test.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/akmyrzza/go-musthave-shortener/internal/config"
-	"github.com/akmyrzza/go-musthave-shortener/internal/repository/pgsql"
+	"github.com/akmyrzza/go-musthave-shortener/internal/mocks"
+	"github.com/akmyrzza/go-musthave-shortener/internal/model"
+	"github.com/golang/mock/gomock"
 	"io"
 	"log"
 	"net/http"
@@ -233,58 +234,55 @@ func TestHandler_CreateIDJSON(t *testing.T) {
 }
 
 func TestHandler_CreateShortURLs(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	cfg, err := config.InitConfig()
-	if err != nil {
-		log.Println(err)
-	}
+	m := mocks.NewMockRepository(ctrl)
 
-	testRepository, err := pgsql.InitDatabase(cfg.DatabasePath)
-	if err != nil {
-		log.Println(err)
-	}
-
-	if testRepository == nil {
-		testRepository, err = repository.NewRepo(cfg.FilePath)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-
-	testService := service.NewServiceURL(testRepository)
+	testService := service.NewServiceURL(m)
 	testHandler := NewHandler(testService, "http://localhost:8080")
 
 	testRouter := gin.Default()
 	testRouter.POST("/api/shorten/batch", testHandler.CreateShortURLs)
 
+	m.EXPECT().CreateShortURLs(gomock.Any()).DoAndReturn(func(req []model.ReqURL) ([]model.ReqURL, error) {
+		return []model.ReqURL{
+			{
+				ID:       "1",
+				ShortURL: "ascnzhjxc",
+			},
+		}, nil
+	}).Times(1)
+
 	type want struct {
 		code int
 	}
 
-	type reqURL struct {
-		ID   string `json:"correlation_id"`
-		Path string `json:"original_url"`
-	}
-
 	tests := []struct {
-		name string
-		url  reqURL
 		want want
 	}{
 		{
-			name: "test #1",
-			url:  reqURL{ID: "1", Path: "www.google.com"},
 			want: want{
 				code: 201,
 			},
 		},
 	}
 
+	type Samples struct {
+		ID           string `json:"correlation_id"`
+		Original_url string `json:"original_url"`
+	}
+
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			jsonData, err := json.Marshal(test.url)
+		t.Run("test mock", func(t *testing.T) {
+			sample := Samples{
+				ID:           "1",
+				Original_url: "www.google.com",
+			}
+
+			jsonData, err := json.Marshal(sample)
 			if err != nil {
-				log.Fatalf("error json: %d", err)
+				log.Println(err)
 			}
 
 			request := httptest.NewRequest(http.MethodPost, "/api/shorten/batch", bytes.NewBuffer(jsonData))
@@ -293,10 +291,25 @@ func TestHandler_CreateShortURLs(t *testing.T) {
 
 			testRouter.ServeHTTP(recorder, request)
 
-			result := recorder.Result()
-			require.NoError(t, result.Body.Close())
+			resp := recorder.Result()
+			require.NoError(t, resp.Body.Close())
+			assert.Equal(t, test.want.code, resp.StatusCode)
 
-			assert.Equal(t, test.want.code, result.StatusCode)
+			//jsonData, err := json.Marshal(test.url)
+			//if err != nil {
+			//	log.Fatalf("error json: %d", err)
+			//}
+			//
+			//request := httptest.NewRequest(http.MethodPost, "/api/shorten/batch", bytes.NewBuffer(jsonData))
+			//request.Header.Set("Content-Type", "application/json")
+			//recorder := httptest.NewRecorder()
+			//
+			//testRouter.ServeHTTP(recorder, request)
+			//
+			//result := recorder.Result()
+			//require.NoError(t, result.Body.Close())
+			//
+			//assert.Equal(t, test.want.code, result.StatusCode)
 		})
 	}
 }
